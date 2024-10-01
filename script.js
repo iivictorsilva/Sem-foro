77d5855fa70c6ec4121e10aeb35a75c70b0a0529
Victor Augusto <vsilva.a.augusto@gmail.com>
2024-10-01 16:44:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let luzAtual = 0;
+function mudarLuz(){
+    
+
+    const vermelho = document.getElementById('vermelho');
+    const amarelo = document.getElementById('amarelo');
+    const verde = document.getElementById('verde');
+
+
+
+    vermelho.classList.remove('active');
+    amarelo.classList.remove('active');
+    verde.classList.remove('active');
+
+
+    if (luzAtual === 0) {
+        vermelho.classList.add('active');
+    } else if (luzAtual === 1){
+        amarelo.classList.add('active');
+    } else if (luzAtual === 2){
+        verde.classList.add('active');
+    }
+
+
+    luzAtual = (luzAtual + 1) % 3;
+
+}
+
+setInterval(mudarLuz, 2000);
